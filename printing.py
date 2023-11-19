a2ee727a68623ef70206e6cb9230057aa77ee692
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-19 15:55:20 +0600

    Create printing.py

---
+++ printing.py
@@ -0,0 +1,10 @@
+#here I wanna print "my name is sumaya" you can print this in different ways
+print("my name is sumaya")
+
+#printing separately
+print("my")
+print("name")
+print("is")
+print("sumaya")
+
+#please note that you can use single quote or double quote to print
