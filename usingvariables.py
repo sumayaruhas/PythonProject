2a224ff2980d95876ae698e72914ce433cefb410
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-20 20:42:02 +0600

    Create usingvariables.py

---
+++ usingvariables.py
@@ -0,0 +1,19 @@
+#using variables in python
+
+#normally printing out
+print("hello my name is jason")
+print("i am 29 years old")
+print("my favourite colour is blue")
+print("i like reading books")
+
+#using variables to print out
+name = "jason"
+age = "29"
+colour = "blue"
+hobby = "books"
+#use + sign to add variables 
+print("hello my name is " + name)
+print("i am " + age + " years old")
+print("my favourite colour is " + colour)
+print("my favourite colour is " + colour)
+print("i like reading " + hobby)
