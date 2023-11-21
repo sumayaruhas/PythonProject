f06bd244e67ce8f7d1c5ed9f6ead35420ccdd7dd
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-21 16:40:41 +0600

    Create using_strings.py

---
+++ using_strings.py
@@ -0,0 +1,47 @@
+#using strings
+
+#print any strings
+
+print("hello , I am printing a string")
+
+#printing length of a string
+
+print(len("hello , I am printing a string"))
+
+#printing a string into upper case
+
+print("hello , i am printing a string".upper())
+
+#printing a string into lower case
+
+print("HELLO , I AM PRINTING A STRING".lower())
+
+#checking if a string is in upper case or lower case
+
+print("hello , i am printing a string".isupper())
+
+print("HELLO , I AM PRINTING A STRING".islower())
+
+print("HELLO , I AM PRINTING A STRING".isupper())
+
+print("hello , i am printing a string".islower())
+
+#assigning a string to a variable
+
+line = "hello , i am printing a string"
+line_1 = "HELLO , I AM PRINTING A STRING"
+print(line)
+
+print(len(line))
+
+print(line.upper())
+
+print(line_1.lower())
+
+print(line.islower())
+
+print(line_1.isupper())
+
+print(line.isupper())
+
+print(line_1.islower())
