2548a5f6d054b2803072fd59b3448b7d3541f33a
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-12-11 23:23:10 +0600

    Create python_inputting.py

---
+++ python_inputting.py
@@ -0,0 +1,14 @@
+# taking input directly 
+
+print(input("what is your name "))
+print(input("what is your age "))
+print(input("what is your favourite food "))
+print(sport = input("what is your favourite sport "))
+
+# using variables
+
+name = input("what is your name ")
+age = input("what is your age ")
+food = input("what is your favourite food ")
+sport = input("what is your favourite sport ")
+print("hello my name is " + name + " i am " + age + " years old . My favourite food is " + food + " my favourite sport is " + sport)
