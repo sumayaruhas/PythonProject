e46376cbdf5e751bd8eeb57b4abadf23ce4cff86
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-12-11 19:22:49 +0600

    Create strings_in_python.py

---
+++ strings_in_python.py
@@ -0,0 +1,15 @@
+# trying to find the index of a character in a string
+
+sentence = "this is my first project"
+
+print(sentence.index("m"))
+print(sentence.index("f"))
+print(sentence.index("r"))
+
+# replacing words in a string
+
+print(sentence.replace("first","second"))
+print(sentence.replace("this","that"))
+
+# printing a string in a new line
+print("hello \nthis is my \nfirst project")
