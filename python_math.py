6ede12daf02bb96b1a55474ec67efbf80a504639
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-12-11 20:02:13 +0600

    Create python_math.py

---
+++ python_math.py
@@ -0,0 +1,48 @@
+# doing mathematics
+
+import math # import math module
+
+x = 2 + 2 # addition
+print(x)
+
+y = 2 - 2 # subtraction
+print(y)
+
+c = 2 * 3 # multiplication
+print(c)
+
+d = 3 / 2 # division
+print(d)
+
+x = max(1 , 2 , 3) # maximum
+y = min(1 , 2 , 3) # minimum
+print(x)
+print(y)
+
+d = abs(-32) # absolute value
+print(d)
+
+a = pow(2 , 3) # power
+print(a)
+
+e = abs(-6.7)
+print(e)
+
+a = math.sqrt(32) # square root
+print(a)
+
+x = math.ceil(2.5) # rounds number upwards
+print(x)
+
+y = math.floor(2.5) # rounds number downwards
+print(y)
+
+x = math.pi # value of pi
+print(x)
+
+
+
+
+
+
+
