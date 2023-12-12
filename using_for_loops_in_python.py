b7de92ace7ad7567dc0aa95b4de91a940493f91d
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-12-12 13:25:39 +0600

    Create using_for_loops_in_python.py

---
+++ using_for_loops_in_python.py
@@ -0,0 +1,25 @@
+# using for loop in python
+
+# printing out each element in a list
+
+letters = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g']
+
+for x in letters:
+    print(x)
+    
+# printing out each element in a string
+
+for x in 'python':
+    print(x)
+
+for x in 'elephant':
+    print(x)
+    
+# printing out in a range
+for x in range(20):
+    print(x)
+
+# printing out in a range with a start and end
+
+for x in range(10 , 20):
+    print(x)
