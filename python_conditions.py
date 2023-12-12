ad199f9f4a3355e9831ef76d947c310ba809bc7d
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-12-12 11:51:44 +0600

    Create python_conditions.py

---
+++ python_conditions.py
@@ -0,0 +1,21 @@
+a = 200
+b = 30
+
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+a = 30
+b = 276
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+    
+a = 70
+b = 30
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
